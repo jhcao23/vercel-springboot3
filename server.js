d5bc2ee22eaab54d7553b34f65e3a2d9788e4b2a
John <jhcao23@gmail.com>
2024-07-10 02:09:18 -0700

    use express

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const app = express();
 const SPRING_BOOT_PORT = 8080;
-const SPRING_BOOT_APP = '/var/task/your-spring-boot-app';
+const SPRING_BOOT_APP = './your-spring-boot-app';
 
 let springBootProcess;
 
